705b0ad79c02c73b726a85f4916b12b1c6044810
susmitha-rajlin <susmitharajlin@gmail.com>
2026-06-09 11:27:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function changeMode() {
+    document.body.classList.toggle("dark");
+}
+
+function validateForm() {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+
+    if (name == "" || email == "") {
+        alert("Please fill all fields");
+        return false;
+    }
+
+    alert("Form Submitted Successfully");
+    return true;
+}
+
+function showTime() {
+
+    let now = new Date();
+
+    document.getElementById("clock").innerHTML =
+        now.toLocaleTimeString();
+}
+
+setInterval(showTime, 1000);
